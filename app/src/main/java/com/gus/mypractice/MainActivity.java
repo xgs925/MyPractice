b59be5ad44c8fe02e2b3bf25930b68701c774009
xgs925 <xgs925@qq.com>
2016-03-29 00:02:46 +0800

    drawerLayout

--- app/src/main/java/com/gus/mypractice/MainActivity.java
+++ app/src/main/java/com/gus/mypractice/MainActivity.java
@@ -1,12 +1,18 @@
 package com.gus.mypractice;
 
+import android.app.ActionBar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,11 +22,12 @@ public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
     @Bind(R.id.drawer)
     DrawerLayout mDrawerLayout;
-    @Bind(R.id.drawer_view)
-    FrameLayout drawView;
+    @Bind(R.id.drawer_view_list)
+    ListView mDrawViewList;
     @Bind(R.id.main_view)
-    FrameLayout mainView;
+    FrameLayout mMainView;
     private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private String[] mDrawerListTitles = {"test1", "test2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,28 @@ public class MainActivity extends AppCompatActivity {
         mActionBarDrawerToggle.syncState();
 
         mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        mDrawViewList.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return mDrawerListTitles.length;
+            }
 
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                convertView = new TextView(MainActivity.this);
+                ((TextView) convertView).setText(mDrawerListTitles[position]);
+                return convertView;
+            }
+        });
     }
 }
