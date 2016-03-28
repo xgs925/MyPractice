5373427bbc6b74a0f61451f4b9dc14369c35aa47
xgs925 <xgs925@qq.com>
2016-03-28 22:28:13 +0800

    drawerLayout

--- app/src/main/java/com/gus/mypractice/MainActivity.java
+++ app/src/main/java/com/gus/mypractice/MainActivity.java
@@ -1,13 +1,42 @@
 package com.gus.mypractice;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.drawer)
+    DrawerLayout mDrawerLayout;
+    @Bind(R.id.drawer_view)
+    FrameLayout drawView;
+    @Bind(R.id.main_view)
+    FrameLayout mainView;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                mToolbar, R.string.app_name, R.string.app_name);
+        mActionBarDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+
     }
 }
